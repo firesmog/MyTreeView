de2963ffaa01b28b9ddb103964028e5bcdcdd080
廖泽宇 <firesmog@163.com>
2020-11-18 15:08:29 +0800

    1.增加json 测试数据

--- app/src/main/java/com/readboy/mytreeview/MainActivity.java
+++ app/src/main/java/com/readboy/mytreeview/MainActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.readboy.mytreeview.utils.AssertsUtil;
+import com.readboy.mytreeview.utils.log.LogUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       text();
+    }
+
+    private void text(){
+        Gson gson = new Gson();
+        String json = AssertsUtil.getFromAssets(MainActivity.this,"json.txt");
+        LogUtils.i("js调用了安卓的方法" + json);
+
     }
 }
